9c66b7578dcc9098bec154519298cb133169d35e
Pablo Otero de miguel <potero-d@c1r7s1.42madrid.com>
2021-09-20 15:33:49 +0200

    libft parte 1

---
+++ libft.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/16 10:21:36 by potero-d          #+#    #+#             */
+/*   Updated: 2021/09/20 15:11:58 by potero-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include<string.h>
+# include<stdlib.h>
+
+void	ft_bzero(void *str, size_t n);
+int		ft_isalnum(int str);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memchr(const void *str, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memset(void *str, int c, size_t len);
+char	*ft_strchr(const char *str, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *str);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *str, const char *nstr, size_t len);
+char	*ft_strrchr(const char *str, int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_atoi(char *str);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
+
+#endif
