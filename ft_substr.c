8173086e09b78fdde807411dd456d6a61e98724b
Pablo Otero <potero91@gmail.com>
2021-12-02 16:08:24 +0100

    new split and substr

--- ft_substr.c
+++ ft_substr.c
@@ -5,38 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/21 10:07:45 by potero-d          #+#    #+#             */
-/*   Updated: 2021/09/21 13:22:02 by potero-d         ###   ########.fr       */
+/*   Created: 2021/12/02 11:05:50 by potero-d          #+#    #+#             */
+/*   Updated: 2021/12/02 16:06:06 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"libft.h"
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
+	size_t	i;
+	size_t	l;
 
 	if (!s)
 		return (0);
-	if (len > ft_strlen(s))
-		len = ft_strlen(s);
-	if (len == 1 && start >= ft_strlen(s))
-	{
-		str = malloc(1);
-		if (!str)
-			return (0);
-		str[0] = '\0';
-		return (str);
-	}
-	else
-		str = malloc(sizeof(char) * (len + 1));
+	l = ft_strlen(s);
+	if (start >= ft_strlen(s))
+		len = 0;
+	if (len > l)
+		len = l;
+	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (0);
-	if (start < ft_strlen(s))
-	{	
-		ft_strlcpy(str, &s[start], len + 1);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
 	}
-	else
-		ft_strlcpy(str, "", 1);
+	str[len] = '\0';
 	return (str);
 }
